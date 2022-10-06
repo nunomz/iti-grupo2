fb03ac94a9b6985ca94dc0d5c8c73d95994639ed
Nuno Moreira <onunomoreira@gmail.com>
2022-10-06 23:56:26 +0100

    Update auto_post_client.py

--- auto_post_client.py
+++ auto_post_client.py
@@ -6,22 +6,21 @@ import requests
 import concurrent.futures
 
 async def file_sender(file_name=None):
-
-    async with aiofiles.open(file_name, 'r') as f:
+    async with aiofiles.open(file_name, 'rb') as f:
         chunk = await f.read(64*1024*1024)
         while chunk:
             yield chunk
             chunk = await f.read(64*1024*1024) 
-    #return chunk
 
 def tester(request):
     url = 'http://0.0.0.0:5000/'
     dir = os.path.join(os.getcwd(), 'imagens/')
     f = random.choice(os.listdir(dir))
     file = os.path.join(dir, f)
-    #print(file)
-    files = open(file, 'rb')
-    response = requests.post('http://0.0.0.0:5000/', data=files, allow_redirects=True)
+    print(file)
+    #files = open(file, 'r')
+    files = file_sender(file)
+    response = requests.post(url, data=files)
     
     if response.status_code == requests.codes.bad:
         print('Headers: {} Response: {}'.format(response.headers, response.text))
@@ -31,7 +30,9 @@ async def main():
     
     async with aiohttp.ClientSession() as session:
         urls = ['http://0.0.0.0:5000/'] * 2
+        print(urls)
         with concurrent.futures.ThreadPoolExecutor(max_workers=2) as pool:
-            pool.map(tester, urls)
+            results = pool.map(tester, urls)
+        print(results)
 
 asyncio.run(main())
