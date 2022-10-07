25baced16c1314164418de0a6febf8db5dea6a1e
PauloBarros-creator <paulo123astro2000@gmail.com>
2022-10-07 01:29:25 +0100

    varios pedidos em simultamio

--- auto_post_client.py
+++ auto_post_client.py
@@ -3,36 +3,57 @@ import asyncio
 import aiofiles
 import os, random
 import requests
-import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
 
-async def file_sender(file_name=None):
-    async with aiofiles.open(file_name, 'rb') as f:
-        chunk = await f.read(64*1024*1024)
-        while chunk:
-            yield chunk
-            chunk = await f.read(64*1024*1024) 
+# async def file_sender(file_name=None):
 
-def tester(request):
-    url = 'http://0.0.0.0:5000/'
-    dir = os.path.join(os.getcwd(), 'imagens/')
-    f = random.choice(os.listdir(dir))
-    file = os.path.join(dir, f)
-    print(file)
-    #files = open(file, 'r')
-    files = file_sender(file)
-    response = requests.post(url, data=files)
+#     async with aiofiles.open(file_name, 'r') as f:
+#         chunk = await f.read(64*1024*1024)
+#         while chunk:
+#             yield chunk
+#             chunk = await f.read(64*1024*1024) 
+#     #return chunk
+
+# form_data = (
+#     "./imagens/divisao.jpg",
+#     "./imagens/heart.jpg",
+#     "./imagens/Schedule.png"
+# )
+
+# async def tester(request):
+#     url = 'http://0.0.0.0:5000/'
+#     dir = os.path.join(os.getcwd(), 'imagens/')
+#     f = random.choice(os.listdir(dir))
+#     file = os.path.join(dir, f)
+#     #print(file)
+#     files = {'file': open(file, 'rb')}
+#     response = await requests.post('url', data=files)
     
-    if response.status_code == requests.codes.bad:
-        print('Headers: {} Response: {}'.format(response.headers, response.text))
+#     if response.status_code == requests.codes.bad:
+#         print('Headers: {} Response: {}'.format(response.headers, response.text))
 
 
-async def main():
+def main(n, url):
     
-    async with aiohttp.ClientSession() as session:
-        urls = ['http://0.0.0.0:5000/'] * 2
-        print(urls)
-        with concurrent.futures.ThreadPoolExecutor(max_workers=2) as pool:
-            results = pool.map(tester, urls)
-        print(results)
+    files = {'file': open(n, 'rb')}
+    data = {'files': 'file', "filename": n}
+    print(files["file"])
+    return requests.post(url, files = files, data = data)
+    # async with aiohttp.ClientSession() as session:
+    #     with await session.post('http://0.0.0.0:5000/', data=n) as resp:
+    #         print(resp.status)
+    #         print(await resp.text())
+        #ficheiro = file_sender(file_name)
+        #async with session.post('http://0.0.0.0:5000/', data = ficheiro) as resp:
+        #    print(resp.status)
+        #    print(await resp.text())
+        #with open(file_name, 'rb') as f:
+        #    await session.post('http://0.0.0.0:5000?Key=file', data=f)
+         
+with ThreadPoolExecutor(max_workers=3) as pool:
+    
+    pool.submit(main,"divisao.jpg", 'http://0.0.0.0:5000/')
+    pool.submit(main,"heart.jpg", 'http://0.0.0.0:5000/')
+    pool.submit(main, "Schedule.png", 'http://0.0.0.0:5000')
 
-asyncio.run(main())
+
---
+++ server.py
@@ -0,0 +1,93 @@
+from crypt import methods
+from flask import Flask, json, request, jsonify, send_from_directory, send_file, current_app, render_template
+import os
+import urllib.request
+import shutil
+import random
+from werkzeug.utils import secure_filename
+ 
+app = Flask(__name__)
+ 
+app.secret_key = "caircocoders-ednalan"
+ 
+MUSIC_FOLDER = 'music'
+UPLOAD_FOLDER = 'uploads' 
+ZIP_FOLDER = 'zips'
+app.config['MUSIC_FOLDER'] = MUSIC_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ZIP_FOLDER'] = ZIP_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 * 1024
+ 
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def get_file_list(input_dir):
+    return [file for file in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, file))]
+
+def get_random_files(file_list, N):
+    return random.sample(file_list, N)
+
+def copy_files(random_files, input_dir, output_dir):
+    for file in random_files:
+        shutil.copy(os.path.join(input_dir, file), output_dir)
+
+#o primeiro nao interessa nao sei porque
+
+# @app.route('/')
+# def main():
+#     print("ola")
+#     return '''
+#     <!doctype html>
+#     <title>Upload new File</title>
+#     <h1>Upload new File</h1>
+#     <form method=post enctype=multipart/form-data>
+#       <input type=file name=file>
+#       <input type=submit value=Upload>
+#     </form>
+#     '''
+
+#
+#
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    print(request)
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        resp = jsonify({'message' : 'No file part in the request'})
+        resp.status_code = 400
+        return resp                                                 
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('No selected file')
+        return Homepage
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename) 
+        #create folder and save file to it
+        foldername = os.path.splitext(filename)[0] #foldername = filename without extension
+        path = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'], foldername)
+        os.mkdir(path)
+        file.save(os.path.join(path, filename))
+        #adds random songs to folder
+        input_dir= os.path.join(current_app.root_path, app.config['MUSIC_FOLDER'])
+        file_list = get_file_list(input_dir) 
+        random_files = get_random_files(file_list, 10)
+        copy_files(random_files, input_dir, path)
+        success = True
+        return render_template('success.html')
+    
+
+@app.route('/download/<path:filename>', methods=['GET'])
+def downloadfile(filename):
+    dir_name = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'], filename) #sets folder to zip
+    output_filename = os.path.join(current_app.root_path, app.config['ZIP_FOLDER'], 'zip_'+filename) #sets desired location and name for zip file
+    shutil.make_archive(output_filename, 'zip', dir_name) #zippity zoppity
+    final_filename = os.path.join(current_app.root_path, app.config['ZIP_FOLDER'], 'zip_'+filename+'.zip') #zip file
+    return send_file(final_filename) #sends zip file
+
+if __name__ == '__main__':
+    app.run(debug=True)
